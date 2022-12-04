07a01b5cf66b323074d33f97d94537e316573bad
Emma McCall <xNymia@users.noreply.github.com>
2022-12-04 15:25:46 +0100

    prepping for day5 @ 30000ft

---
+++ Day5/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <utility>
+#include <tuple>
+
+int main() {
+
+    std::ifstream inputFile("input.list");
+    std::string inputLine;
+
+    while(getline(inputFile, inputLine)) {
+
+
+    }
+
+    std::cin.get();
+}
